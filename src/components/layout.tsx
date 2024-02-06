e7bc5b85102e96037f0a4c6ebf2b65acc771dd57
4SAMU <samuelnzomo4@gmail.com>
2024-02-06 14:04:09 +0300

    product data context

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,9 @@
 import { MainComponent, PageContainer } from "@/styles/common.styles";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeadMetaData from "./HeadMetadata";
 import SidebarComponent from "./common-components/navbar/sidebar";
 import TopbarComponent from "./common-components/navbar/topbar";
+import { ProductDataProvider } from "@/utils/context/products-data";
 
 interface layoutProps {
   children: any;
@@ -11,25 +12,38 @@ interface layoutProps {
 }
 
 const Layout = ({ children, pageTitle, showSearchComponent }: layoutProps) => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/service");
+      const data = await response.json();
+      const { services } = data;
+      setServices(services);
+    })();
+  }, []);
+
   return (
-    <PageContainer>
-      <HeadMetaData pageTitle={pageTitle} />
-      <SidebarComponent />
-      <MainComponent>
-        <TopbarComponent showSearchComponent={showSearchComponent} />
-        <main
-          style={{
-            position: "relative",
-            height: "calc(100% - 64px)",
-            overflowY: "auto",
-            overflowX: "hidden",
-            marginBottom: "20px",
-          }}
-        >
-          {children}
-        </main>
-      </MainComponent>
-    </PageContainer>
+    <ProductDataProvider value={services}>
+      <PageContainer>
+        <HeadMetaData pageTitle={pageTitle} />
+        <SidebarComponent />
+        <MainComponent>
+          <TopbarComponent showSearchComponent={showSearchComponent} />
+          <main
+            style={{
+              position: "relative",
+              height: "calc(100% - 64px)",
+              overflowY: "auto",
+              overflowX: "hidden",
+              marginBottom: "20px",
+            }}
+          >
+            {children}
+          </main>
+        </MainComponent>
+      </PageContainer>
+    </ProductDataProvider>
   );
 };
 
