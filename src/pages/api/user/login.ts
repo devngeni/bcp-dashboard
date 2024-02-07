7a6c76ff6874a1a45939effcef59830aebd70385
Hakizimana Fred <hakifred20@gmail.com>
2024-02-07 11:53:47 +0300

    admin auth methods

--- src/pages/api/user/login.ts
+++ src/pages/api/user/login.ts
@@ -20,7 +20,6 @@ export default async function handler(
         return res.status(200).json({ user: user.toJson() });
       } else {
         console.log("Invalid credentials");
-        // we shall discuss if we will need sessions or jwt once we start implementing Oauth part - Fred
         res.status(400).json({ message: "authentication of admin failed" });
       }
     } catch (e: any) {
