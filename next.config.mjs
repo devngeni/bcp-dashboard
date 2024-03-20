e8e25db0dc11517e62a22cd574a0ad1e3e758e02
4SAMU <samuelnzomo4@gmail.com>
2024-03-20 14:39:19 +0300

    cors errors

--- next.config.mjs
+++ next.config.mjs
@@ -21,20 +21,20 @@ const nextConfig = {
         headers: [
           // Allow for specific domains to have access or * for all
           {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-            // DOES NOT WORK
-            // value: process.env.ALLOWED_ORIGIN,
-          },
-          // Allows for specific methods accepted
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
+            source: "/api/(.*)",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET,DELETE,PATCH,POST,PUT",
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value:
+                  "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              },
+            ],
           },
         ],
       },
