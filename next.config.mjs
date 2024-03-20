ec6c66224deaa46bfff37a04b9d0dc189ddf8980
4SAMU <samuelnzomo4@gmail.com>
2024-03-20 14:41:56 +0300

    fix deploy error

--- next.config.mjs
+++ next.config.mjs
@@ -15,26 +15,18 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Routes this applies to
-        source: "/api/(.*)",
-        // Headers
+        source: "/api/:path*",
         headers: [
-          // Allow for specific domains to have access or * for all
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
-            source: "/api/(.*)",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              {
-                key: "Access-Control-Allow-Methods",
-                value: "GET,DELETE,PATCH,POST,PUT",
-              },
-              {
-                key: "Access-Control-Allow-Headers",
-                value:
-                  "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-              },
-            ],
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
